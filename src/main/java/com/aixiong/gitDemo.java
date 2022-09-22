187438645f5ee238edbd71ef36cb933e8ca9dbbb
aixiong <2574055941.@qq.com>
2022-09-22 09:49:15 +0800

    third commit

--- src/main/java/com/aixiong/gitDemo.java
+++ src/main/java/com/aixiong/gitDemo.java
@@ -4,5 +4,6 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("hell aixiong");
         System.out.println("hello git" );
+        System.out.println("hello xiong");
     }
 }
