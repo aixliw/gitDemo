c66e4eac2b8c075121c45b40e689e2b1c4014a75
aixiong <2574055941.@qq.com>
2022-09-22 09:48:27 +0800

    third commit

--- src/main/java/com/aixiong/aiDemo.java
+++
@@ -1,7 +0,0 @@
-package com.aixiong;
-
-public class aiDemo {
-    public static void main(String[] args) {
-        System.out.println("hello git");
-    }
-}
--- src/main/java/com/aixiong/gitDemo.java
+++ src/main/java/com/aixiong/gitDemo.java
@@ -3,5 +3,6 @@ package com.aixiong;
 public class gitDemo {
     public static void main(String[] args) {
         System.out.println("hell aixiong");
+        System.out.println("hello git" );
     }
 }
